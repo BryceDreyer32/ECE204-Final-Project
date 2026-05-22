64b5af16c44ad9c19841065c0414c745b3711bbc
Bryce <bryce.b.dreyer@gmail.com>
2026-05-22 09:35:55 -0700

    Fixed output timing of acc_out in pe core

--- rtl/pe.sv
+++ rtl/pe.sv
@@ -14,14 +14,14 @@ logic signed [31:0] acc; // Internal register to hold the accumulated value, sep
 logic signed [WIDTH * 2 - 1:0] product;        // 16 bit wide output, since we're multiplying two 8 bit inputs
 logic signed [WIDTH * 4 - 1:0] product_ext;    // MSB for signed binary is the sign bit. Replicating the sign bit left of the number doesn't change the number since it's two's complement
 
-always_comb begin                            // Not sequential, just an intermediate connection extended
+always_comb begin
+    acc_out = en ? acc_in : acc;          
     product     = a_in * b_in;
-    product_ext = 32'($signed(product));     // "$signed" tells SystemVerilog to treat a given signal as a two's complement number, the prefix dictates the bit width
+    product_ext = 32'($signed(product));        // "$signed" tells SystemVerilog to treat a given signal as a two's complement number, the prefix dictates the bit width
 end
 
 always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin                   // Explicitly matched bit width reset signals
-        acc_out <= 32'sd0;
         acc     <= 32'sd0;
         a_out   <= '0;
         b_out   <= '0;
@@ -29,15 +29,12 @@ always_ff @(posedge clk or negedge rst_n) begin
     end 
     
     else begin                          // Passes input to output in one clock cycle
-        if(en) begin
+        if(en) 
             acc <= acc + product_ext;   // product_ext is the full product, this is just the addition of components
-            acc_out <= acc_in;          // Shift result from previous PE
-        end
-
-        else begin
-            acc_out <= acc;             // Place current value on the output 
+        
+        else 
             acc <= 32'sd0;              // Clear accumulator for next round of computation    
-        end
+        
         a_out   <= a_in;
         b_out   <= b_in;
         en_out  <= en;
